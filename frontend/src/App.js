058e9ebc68c764b8c4513095132dafeca82977ba
Sivadeep <166890093+Sivadeep30@users.noreply.github.com>
2025-01-30 22:56:36 +0530

    Banner creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner/>
       
     </div>
   );
