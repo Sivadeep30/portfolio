4738f611aae578868b0402c6a796a3694a336289
Sivadeep <166890093+Sivadeep30@users.noreply.github.com>
2025-02-05 22:18:52 +0530

    projects section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Banner/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
