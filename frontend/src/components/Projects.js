3504255c5d0a2c194c7974a41fadf253373b6272
Sivadeep <166890093+Sivadeep30@users.noreply.github.com>
2025-02-08 16:11:16 +0530

    simple changes

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -11,8 +11,8 @@ import TrackVisibility from 'react-on-screen';
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Online Automobile",
+      description: "Tech Stack\n: React\n- Node.js\n- MongoDB",
       imgUrl: projImg1,
     },
     {
@@ -51,17 +51,16 @@ import TrackVisibility from 'react-on-screen';
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Full Stack</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">AI/ML</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">IOT</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -79,7 +78,7 @@ import TrackVisibility from 'react-on-screen';
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
+                    <Tab.Pane eventKey="second">
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
